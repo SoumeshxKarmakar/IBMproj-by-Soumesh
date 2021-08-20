50984420fb1d98ad521f86f9b97e739f4d891f76
SoumeshxKarmakar <89100397+SoumeshxKarmakar@users.noreply.github.com>
2021-08-20 22:20:14 +0530

    Update script.js
    initial change

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate of interest").value;
+    t = document.getElementById("time").value;
+    s = document.getElementById("Result");
+    result.innerHTML = "Simple interest is =" + (p*r*t/100);
     
 }
         
